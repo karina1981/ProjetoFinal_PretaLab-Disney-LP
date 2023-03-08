cd532d0d46ab4f1b7b3da60502e152458f304544
karina_ros <karina.souza1981@gmail.com>
2023-03-07 21:55:17 -0300

    Implementação calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -4,12 +4,12 @@ function somar() {
   // evite mexer no código acima!
 
 
-
+  const result = (numero1 + numero2);
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = result;
 }
 
 function subtrair() {
@@ -17,13 +17,11 @@ function subtrair() {
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
   // evite mexer no código acima!
 
-
-
-
+  const result = numero1 - numero2;
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
-  document.getElementById("resultadoSub").innerHTML = 9;
+  document.getElementById("resultadoSub").innerHTML = result;
 }
 
 function multiplicar() {
@@ -31,13 +29,11 @@ function multiplicar() {
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
   // evite mexer no código acima!
 
-
-
-
+  const result = numero1 * numero2;
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
-  document.getElementById("resultadoMult").innerHTML = 5;
+  document.getElementById("resultadoMult").innerHTML = result;
 }
 
 function dividir() {
@@ -45,11 +41,9 @@ function dividir() {
   const numero2 = parseFloat(document.getElementById("numero2Div").value);
   // evite mexer no código acima!
 
-
-
-
+  const result = numero1 / numero2;
   
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
-  document.getElementById("resultadoDiv").innerHTML = -89;
+  document.getElementById("resultadoDiv").innerHTML = result;
 }
